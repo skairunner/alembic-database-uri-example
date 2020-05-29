220007f9f0d54aec77cdf02b633e6a0cf3326723
Skye Im <skye.im@cognite.com>
2020-05-29 15:13:58 +0200

    Initial example

---
+++ alembic/versions/bcc6eb6d4ba9_initial.py
@@ -0,0 +1,24 @@
+"""initial
+
+Revision ID: bcc6eb6d4ba9
+Revises: 
+Create Date: 2020-05-29 15:10:08.201433
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'bcc6eb6d4ba9'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
